331caf08e4a39aaf794d002d4c92bb09cb5a5605
Francesco Manenti <francesco@manenti.net>
2025-09-03 21:06:44 +0200

    manage space pressed to close the app

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
 const path = require("path");
 
 let mainWindow;
@@ -8,9 +8,40 @@ app.on("ready", () => {
         webPreferences: {
             nodeIntegration: true,
         },
+        show: false
     });
     mainWindow.maximize();
+    mainWindow.show();
     mainWindow.fullScreen = true;
 
     mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+    // Gestione chiusura in fullscreen
+    mainWindow.on('close', (e) => {
+        if (mainWindow.isFullScreen()) {
+            // Esci dal fullscreen prima di chiudere
+            mainWindow.setFullScreen(false);
+            // Previeni la chiusura, verrà richiamata una volta fuori dal fullscreen
+            e.preventDefault();
+            // Aspetta che sia uscito dal fullscreen e chiudi davvero la finestra
+            mainWindow.once('leave-full-screen', () => {
+                mainWindow.close();
+            });
+        }
+    });
+
+    globalShortcut.register('Escape', () => {
+        // Get the currently focused window
+        const focusedWindow = BrowserWindow.getFocusedWindow();
+        if (focusedWindow && focusedWindow.isFullScreen()) {
+            focusedWindow.setFullScreen(false);
+        }
+    });
+
+    globalShortcut.register('Space', () => {
+        // Get the currently focused window
+        const focusedWindow = BrowserWindow.getFocusedWindow();
+        focusedWindow.close();
+    });
+
 });
